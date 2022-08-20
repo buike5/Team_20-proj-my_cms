f9a176a03d7ba627bfd2219e560b00941aade400
ikechukwu mmachukwu <ikechukwummachukwu2020@gmail.com>
2022-08-20 17:36:11 +0100

    changes

--- Dsyne/settings.py
+++ Dsyne/settings.py
@@ -150,7 +150,7 @@ WSGI_APPLICATION = "Dsyne.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.postgresql",
     }
 }
 # else:
